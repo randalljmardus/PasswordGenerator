cd6b3f64f465afa0b33a96120eb40f9078d00b40
Randall Mardus <randalljmardus@gmail.com>
2016-05-12 11:51:32 -0400

    initial commit

---
+++ MainWindowController.swift
@@ -0,0 +1,13 @@
+//
+//  MainWindowController.swift
+//  RandomPassword
+//
+//  Created by Randall Mardus on 5/2/16.
+//  Copyright © 2016 Randall Mardus. All rights reserved.
+//
+
+import UIKit
+
+class MainWindowController: NSWindowController {
+
+}
